a95a98c10c21ecac5af05b0e94d73327eeaf913b
fayyazbinfida <khanfayyaz45@gmail.com>
2024-02-26 19:15:37 +0500

    Updated

--- README.md
+++
@@ -1,1 +0,0 @@
-# brixbull.funnel
--- founders-special/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  $(".features").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".categories-outer").offset().top},
-        'slow');
-});
-});
